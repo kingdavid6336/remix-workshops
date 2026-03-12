3cbe44bcc38fb17d8e42add18a3d9482aec82e81
davidekete <eketedavid@gmail.com>
2026-03-12 01:19:51 +0100

    tutorial review

--- Centralization/centralization/Centralization.sol
+++ Centralization/centralization/Centralization.sol
@@ -5,10 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Centralization is ERC20, Ownable {
-    constructor() ERC20("Centralization", "Cent") {
-        address exposedAccount = 0xe16C1623c1AA7D919cd2241d8b36d9E79C1Be2A2;
-        transferOwnership(exposedAccount);
-    }
+    // NOTE: In a real attack scenario, the owner's private key is leaked or compromised
+    // (e.g. 0xe16C1623c1AA7D919cd2241d8b36d9E79C1Be2A2), allowing an attacker to call mint().
+    constructor() ERC20("Centralization", "Cent") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) external onlyOwner{
         _mint(to, amount);
